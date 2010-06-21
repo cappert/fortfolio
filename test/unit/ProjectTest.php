3508cfe58523023ab78d5fd788220b4089cf5219
Camille Appert <camille@appert.me>
2010-06-21 16:45:30 +0200

    adding proper test fixtures

--- test/unit/ProjectTest.php
+++ test/unit/ProjectTest.php
@@ -4,7 +4,7 @@ require_once dirname(__FILE__).'/../bootstrap/unit.php';
 
 $configuration = ProjectConfiguration::getApplicationConfiguration( 'frontend', 'test', true);
 new sfDatabaseManager($configuration);
-Doctrine_Core::loadData(sfConfig::get('sf_test_dir').'/test');
+Doctrine_Core::loadData(sfConfig::get('sf_test_dir').'/fixtures');
 
 $t = new lime_test(3);
 
